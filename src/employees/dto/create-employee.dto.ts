7e455de01893356fa43fa069302a279eec7828cf
Nestor Daniel Gomez <nestordanielgomez@gmail.com>
2023-11-30 12:08:44 -0300

    dto employee

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,1 +1,20 @@
-export class CreateEmployeeDto {}
+import { IsNumber, IsString, MaxLength, MinLength } from "class-validator"
+
+export class CreateEmployeeDto {
+
+    @IsNumber()
+    @MinLength(8)
+    @MaxLength(8)
+    dni: number
+
+    @IsNumber()
+    edad: number
+
+    @IsString()
+    @MinLength(3)
+    nombre: string
+
+    @IsString()
+    @IsNumber()
+    cargo: string
+}
