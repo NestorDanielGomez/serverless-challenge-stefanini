b3262ed8dfaf8b94f8d389f94ec1facd7d8f4ff0
Nestor Daniel Gomez <nestordanielgomez@gmail.com>
2023-11-30 12:04:44 -0300

    create entity employee

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,1 +1,20 @@
-export class Employee {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Employee {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column("text", { unique: true })
+    dni: number
+
+    @Column("number")
+    edad: number
+
+    @Column("text")
+    nombre: string
+
+    @Column("text")
+    cargo: string
+}
