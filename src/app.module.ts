46e4ed5d683bdc032ee2ff89d4fd036ec8b722bd
Nestor Daniel Gomez <nestordanielgomez@gmail.com>
2023-11-30 21:57:35 -0300

    test from aws

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,12 @@ import { EmployeesModule } from './employees/employees.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === "prod" ? true : false,
+      extra: {
+        ssl: process.env.STAGE === "prod"
+          ? { rejectUnauthorized: false }
+          : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
